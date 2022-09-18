8d357fc94832e994443c9ed7d98cba43ae35c76f
idil bulak <ib@idils-MacBook-Pro-2.local>
2022-09-18 15:11:42 +0200

    fixed

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -14,29 +14,37 @@ Harl::~Harl( void ) {
 
 void	Harl::debug( void ) {
 
-	std::cout << GREY << "Level : DEBUG" << RESET << std::endl;
-	std::cout << GREY << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << RESET << std::endl;
+	std::cout << GREY << "[ DEBUG ]" << RESET << std::endl;
+	std::cout << GREY << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl;
+	std::cout << "I really do!" << RESET << std::endl;
+	std::cout << std::endl;
 	return;
 }
 
 void	Harl::info( void ) {
 
-	std::cout << GREY << "Level : INFO" << RESET << std::endl;
-	std::cout << GREY << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << RESET << std::endl;
+	std::cout << GREY << "[ INFO ]" << RESET << std::endl;
+	std::cout << GREY << "I cannot believe adding extra bacon costs more money." << std::endl;
+	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << RESET << std::endl;
+	std::cout << std::endl;
 	return;
 }
 
 void	Harl::warning( void ) {
 
-	std::cout << GREY << "Level : WARNING" << RESET << std::endl;
-	std::cout << GREY << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << RESET << std::endl;
+	std::cout << GREY << "[ WARNING ]" << RESET << std::endl;
+	std::cout << GREY << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming for years whereas you started working here since last month." << RESET << std::endl;
+	std::cout << std::endl;
 	return;
 }
 
 void	Harl::error( void ) {
 
-	std::cout << GREY << "Level : ERROR" << RESET << std::endl;
-	std::cout << GREY << "This is unacceptable! I want to speak to the manager now." << RESET << std::endl;
+	std::cout << GREY << "[ ERROR ]" << RESET << std::endl;
+	std::cout << GREY << "This is unacceptable!" << std::endl;
+	std::cout << "I want to speak to the manager now." << RESET << std::endl;
+	std::cout << std::endl;
 	return;
 }
 
