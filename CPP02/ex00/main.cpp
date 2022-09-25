2289d85e97b358e91fc7d7c64585fc69f876eef0
idil bulak <ib@idils-MacBook-Pro-2.local>
2022-09-25 14:02:39 +0200

    Makefile

--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -12,7 +12,6 @@ int main( void ) {
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
     std::cout << std::endl;
-    std::cout << setfil<< std::endl;
 
     a.setRawBits(8);
     b.setRawBits(8);
